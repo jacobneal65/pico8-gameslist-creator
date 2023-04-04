87ac575bcd1dba2ef6d1d5fe04df436dd532ee63
Jacob <jacobneal65@gmail.com>
2023-04-03 23:05:44 -0500

    finished basic script

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ a small python script that creates &lt;game> xml entries for emulation station's
 The plan is that it will use a folder of pico8 carts to create a basic xml game structure and 
 
 1) set the &lt;image> to the .png cart.
-2) make the &lt;name> the file name.
+2) make the &lt;name> the file name. 
+3) set the &lt;path> to the .png cart.
--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 from xml.etree import ElementTree
 import time, glob, sys, re, os
+import xml.dom.minidom
 
 
 def pico_xml_parse():
-    print("begin xml parse:")
-    xml_path = os.path.expanduser('~\\Desktop\\gamelist.xml') #Expand user starts in the user's folder.
-    xml = ElementTree.parse(xml_path)
-    for game in xml.findall('game'):
-        image = game.findtext('image')
+    xml_path = os.path.expanduser('~\\Desktop\\temp_gamelist.xml')  # Expand user starts in the user's folder.
+    # xml = ElementTree.parse(xml_path)
+
+    # grab the name of all the files
+    carts = glob.glob(os.path.expanduser('~\\testgamelistforpico8\\*'))
+
+    # - Create new XML document with its root element
+    doc = xml.dom.minidom.parseString("<gameList/>")
+    root = doc.documentElement
+
+    # -----------------------------------------------------
+    for cart in carts:
+        cart_name = os.path.basename(cart)
+
+        game_name = re.sub(r'\.p8.png$', '', cart_name)  # (r=raw string) (a,b,c) replace a with b in string c
+
+        # game_name = os.path.splitext(cart_name)
+        game = doc.createElement('game')
+        # - Add new element to root
+        root.appendChild(game)
+
+        # - Create path element with text
+        path = doc.createElement('path')
+        path_text = doc.createTextNode(f'./carts/{cart_name}')
+        path.appendChild(path_text)
+        game.appendChild(path)
+
+        # - Create name element with text
+        path = doc.createElement('name')
+        path_text = doc.createTextNode(game_name)
+        path.appendChild(path_text)
+        game.appendChild(path)
+
+        # - Create image element with text
+        path = doc.createElement('image')
+        path_text = doc.createTextNode(f'./carts/{cart_name}')
+        path.appendChild(path_text)
+        game.appendChild(path)
+
+    # prettify the xml
+    xml_str = root.toprettyxml(indent="\t")
+    # write the xml to the file
+    print(xml_str)
+    with open(xml_path, "w") as f:
+        f.write(xml_str)
+
+    # -----------------------------------------------------
 
 
 if __name__ == '__main__':
     pico_xml_parse()
-    # test
-
-# <?xml version="1.0"?>
-# <game>
-# 		<path>./carts/ascent-0.p8.png</path>
-# 		<name>Ascent</name>
-# 		<image>./carts/ascent-0.p8.png</image>
-# 		<desc>Crashed on a desolate but mysterious planet you find yourself eye to eye with an ancient civilisation.
-# 			Explore the planet, find powerful upgrades, and uncover its secrets.
-#
-# 			CONTROLS
-# 			move keys
-# 			jump o
-# 			special moves x
-# 		</desc>
-# 		<genre>Platformer</genre>
-# 		<lang>en</lang>
-# </game>
-
-
